099ce6fb460cb9efce14a383717ad3bac9a04d2c
ruodgers <ruodgers@xgs0r.onmicrosoft.com>
2024-12-26 21:10:59 -0300

    adicionei o main

---
+++ main.py
@@ -0,0 +1,26 @@
+#Meu primeiro arquivo python
+nome = "raimundo"
+print(nome)
+
+#valores
+idade = 33
+print(idade)
+
+#calculos
+idade_nova = idade + 1
+print(idade_nova)
+
+#listas
+lista1 = [1,2,3,4,5]
+print(lista1)
+
+#funções
+#lista_invertida = lista1.reversed()
+#print(lista_invertida)
+
+lista1 = [1, 2, 3, 4, 5]
+print(lista1)
+
+#lista invertida
+lista_invertida = list(reversed(lista1))
+print(lista_invertida)
